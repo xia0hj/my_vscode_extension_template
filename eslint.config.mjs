6d1a5ec52c182960d533b5c814cb91256ed1b9db
xia0hj <719887163@qq.com>
2024-09-08 20:07:27 +0800

    😁 add vitest example

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,13 +4,6 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import stylistic from "@stylistic/eslint-plugin";
 
-/**
- * This tells VS Code to not show errors in your editor, but still have the ability to auto-fix them.
- * You can also set "severity": "info" to keep squiggly lines but not have them look like errors.
- * @example
- *
- */
-
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
@@ -26,21 +19,23 @@ export default tseslint.config(
             "@typescript-eslint/no-unused-vars": "warn",
         },
     },
+    {
+        ignores: [
+            "dist/",
+        ],
+    },
 );
 
 /**
  * This tells VS Code to not show errors in your editor, but still have the ability to auto-fix them.
  * You can also set "severity": "info" to keep squiggly lines but not have them look like errors.
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const ignoreStylisticHint = `
-{
-  "eslint.rules.customizations": [
-    {
-      "rule": "@stylistic/*",
-      "fixable": true,
-      "severity": "off"
-    }
-  ]
-}
-`;
+// {
+//   "eslint.rules.customizations": [
+//     {
+//       "rule": "@stylistic/*",
+//       "fixable": true,
+//       "severity": "off"
+//     }
+//   ]
+// }
